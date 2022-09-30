84c7403d24dba5aa183785b02b6f942ad1f3fbe6
Gaszoelina <gasza.mendy@gmail.com>
2022-09-30 16:38:55 +0000

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
-nt(roman_string: str):
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
     if roman_string is None or type(roman_string) != str:
         return 0
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -11,4 +12,4 @@ nt(roman_string: str):
         else:
             rep -= numbers[i]
 
-    return 
+    return rep
